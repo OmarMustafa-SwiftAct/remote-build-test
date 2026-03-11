5f6eaa7acebdf66130c75d715f8a480908f3fba7
OmarMustafa-SwiftAct <Omar.Mustafa@swift-act.com>
2026-03-11 06:36:44 +0200

    Remote Build Trigger

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-    printf("Hello from the Shadow Build!\n");
+    printf("Hello from the Shadow Build build actually works!, but it deletes local changes!\n");
     return 0;
 }
